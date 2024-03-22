d0e3a7f518163bff45c0cb2a39eac9a3b82bc95c
hamoud <chaarimahmoud55@gmail.com>
2024-03-22 23:21:06 +0100

    added RoomFilter and RoomPaginator components

---
+++ Front/marina-hotel/src/components/common/RoomFilter.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const RoomFilter =({data,setFilteredData}) =>{
+    const[filter,setFilter] =useState("")
+    const handleSelectChange =(e) => {
+        const selectedRoomType= e.target.value
+        setFilter(selectedRoomType)
+        const filtredRooms = data.filter((room) => 
+        room.roomType.toLowerCase().includes(selectedRoomType.toLowerCase()))
+        setFilteredData(filtredRooms)
+    }
+
+
+const clearFlter = ()=>{
+    setFilter("")
+    setFilteredData(data)
+
+}
+
+const roomTypes =["",...new Set(data.map((room) => room.roomType))]
+    return(
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="room-type-filter">
+                Filter rooms by type
+            </span>
+            <select className="form-select"
+            value={filter}
+            onChange={handleSelectChange}
+            >
+                <option value={""}>select a room type to filter ...</option>
+                {roomTypes.map((type,index)=>(
+                  <option key={index} value={String(type)}>
+                    {type}
+                  </option>  
+                ))}
+            </select>
+           <button className="btn btn-hotel" type="button" onClick={clearFlter}>
+            Clear Filter
+            </button> 
+
+        </div>
+    )
+}
+export default RoomFilter
---
+++ Front/marina-hotel/src/components/common/RoomPaginator.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const RoomPaginator =({currentPage,totalPages,onPageChange}) => {
+    const pageNumbers = Array.from({length:totalPages},(_,i) => i+1)
+    return(
+        <nav>
+            <ul className="pagination,justify-content-center">
+                {pageNumbers.map((pageNumber) => (
+                    <li key={pageNumber}
+                    className={`page-item ${currentPage === pageNumber ? "active" : ""}`}
+                    >
+                        <button className="page-link" onClick={()=>onPageChange(pageNumber)}>
+                            {pageNumber}
+                        </button>
+
+                    </li>
+                ))}
+
+            </ul>
+
+        </nav>
+    )
+
+
+}
+export default RoomPaginator
