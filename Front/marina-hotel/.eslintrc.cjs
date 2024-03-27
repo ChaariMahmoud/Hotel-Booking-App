fec09d4313665e334b6b5a4af0db35624d05bec4
hamoud <chaarimahmoud55@gmail.com>
2024-03-27 16:31:02 +0100

    Re-configuration

--- Front/marina-hotel/.eslintrc.cjs
+++ Front/marina-hotel/.eslintrc.cjs
@@ -2,25 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'eslint:recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    "react/prop-types": "off",
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true ,
-        allowExportNames: ["loader"],},
+      { allowConstantExport: true },
     ],
-    "react/jsx-uses-react": "error",   
-     "react/jsx-uses-vars": "error" 
   },
 }
